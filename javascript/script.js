2ec1a6d283999479fec0e10bf6792c29bdac8eb3
DarkyRobin <velascoayen@gmail.com>
2023-07-27 23:04:16 +1000

    New activity

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// const animal = {
+//     animalType: 'dog',
+//     breed: 'Shipoo',
+//     age: '8',
+//     catFriendly: false,
+//     petName: 'Goku',
+//     sayHi: function(){
+//         return (`Henlo there! I'm ${this.petName}, an ${this.age}-year-old ${this.breed}.`)
+//     } 
+// }
+
+// console.log(animal.sayHi());
+// console.log(`Am I cat friendly? ${animal['catFriendly']}`);
+
+// let newFlower = {
+//     name: 'sunflower',
+//     origin: 'Europe'
+// }
+// let gardenPlants = [ 
+//     {
+//         name: 'roses', 
+//         origin: 'China'
+//     },
+//     {
+//         name: 'tulips', 
+//         origin: 'Asia'
+//     },
+// ]
+// console.log(gardenPlants.unshift(newFlower))
+let die = Math.floor(Math.random() * 6 + 1);
+console.log(die)
--- prod_landing_page/index.js
+++ prod_landing_page/index.js
@@ -16,3 +16,27 @@ window.onscroll = () => {
     burger_menu.classList.remove('bx-x');
     menu_list.classList.remove('open');
 }
+
+let home_title = document.getElementById('home_title');
+let home = document.getElementById('home');
+console.log(home_title);
+
+let allH1Elements = document.getElementsByTagName('h1');
+console.log(allH1Elements);
+
+home_title.innerText = 'Your have been hacked! Pay me b*tch.';
+home_title.style.color = "magenta";
+
+let div = document.createElement('div');
+let hackedButton = document.createElement('button');
+hackedButton.setAttribute('id', 'hacked_button');
+hackedButton.textContent = 'Pay me here';
+hackedButton.style.width = '100%';
+hackedButton.style.backgroundColor = 'red';
+hackedButton.style.color = 'yellow'; 
+home.appendChild(div);
+div.appendChild(hackedButton);
+hackedButton.onclick = () => {
+    hackedButton.style.backgroundColor = 'black';
+    hackedButton.style.color = 'white'; 
+}
