9adbc7469cadfcd0333aaa08d220956d7c1b13cc
DarkyRobin <velascoayen@gmail.com>
2023-08-15 18:32:21 +0800

    Update on tic tac toe

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -5,34 +5,75 @@ const resetBtn = document.getElementById('resetButton');
 const prevtBtn =document.getElementById('prevButton'); 
 const nextBtn =document.getElementById('nextButton');
 let player = document.getElementById('player');
-let curPlayer  = player.value;
+let curPlayer  = 'X';
+player.addEventListener('change', function(){
+  curPlayer = player.value;
+  infoGame.innerHTML = `Player ${curPlayer} Start`;
+})
+
 const winningCombos = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
   [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
   [0, 4, 8], [2, 4, 6]           // diagonals
 ];
 
-let startCells = ["", "", "", "", "", "", "", "", ""];
-let isGameRunning = false;
+let boardCells = ["", "", "", "", "", "", "", "", ""];
+let isGameOver = false;
 
-function initGame() {
-  cells.forEach(cell => cell.addEventListener('click', clickedCell));
-  resetBtn.addEventListener('click', resetGame);
-  infoGame.textContent = `${curPlayer}'s Turn`;
+divticTacToe.addEventListener('click', markCellTarget)
+
+function playerTurn(){
+  curPlayer = curPlayer === 'X' ? 'O' : 'X';
+  infoGame.innerHTML = `Player ${curPlayer}'s Turn`;
 }
 
-function clickedCell() {
-  const cellIndex = this.getAttribute(id);
-  console.log()
+function updateBoardCells(cellId){
+  boardCells[cellId] = curPlayer;
+}
+
+function markCellTarget(e) {
+  const target = e.target;
+  const targetValue = document.getElementById(target.id);
+  targetValue.innerText = curPlayer;
+  updateBoardCells(target.id);
+  checkWinner();
+  console.log(curPlayer)
+  
 }
 
 function checkWinner() {
+  for(let i = 0; i <=7; i++) {
+    const winCombo = winningCombos[i];
+    let a = boardCells[winCombo[0]];
+    let b = boardCells[winCombo[1]];
+    let c = boardCells[winCombo[2]];
 
+    if (a === '' || b === '' || c === '') {
+      continue;
+    }
+    
+    if (a === b && b === c) {
+      isGameOver = true;
+      break
+    }
+  }
+  
+  if(isGameOver) {
+    infoGame.innerHTML = `Player ${curPlayer}'s Won!`;
+    return;
+  } else {
+    playerTurn();
+  }
 }
-
-
+  
 
 function resetGame() {
-
+  cells.forEach((cell, index) => {
+    cell.innerText = '';
+  })
+  boardCells = ["", "", "", "", "", "", "", "", ""];
+  infoGame.innerHTML = `Select player`;
 }
+
+resetBtn.addEventListener('click', resetGame);
 
